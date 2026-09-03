edbeee72eeefadf96c969953decd18e464219fa0
Rodrigo Justo <Rodrigojy20@gmail.com>
2026-09-03 16:55:37 -0500

    Crear proyecto inicial de GroupOrder

---
+++ src/main/java/com/grouporder/GrouporderApplication.java
@@ -0,0 +1,13 @@
+package com.grouporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouporderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouporderApplication.class, args);
+	}
+
+}
